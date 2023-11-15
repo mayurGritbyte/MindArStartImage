dc0c24139c1dc6c24ea203b86563b5f127c3c21b
mayurGritbyte <127743503+mayurGritbyte@users.noreply.github.com>
2023-11-15 17:53:43 +0530

    Update main.js

--- Imagetracking/startImagetracking/main.js
+++ Imagetracking/startImagetracking/main.js
@@ -23,3 +23,32 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     start();
 });
+
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    // initialize MindAR 
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: './testMindARGritbyteLabs.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    // create AR object
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+    const plane = new THREE.Mesh(geometry, material);
+
+    // create anchor
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(plane);
+
+    // start AR
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
